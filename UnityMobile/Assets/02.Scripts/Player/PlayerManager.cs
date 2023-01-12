ca7b111a35e2be59955a088a9cb59a1182294975
KIM JUNGHYUN <wjdgusrla125@gmail.com>
2023-01-12 21:51:48 +0900

    캐릭터 수정 및 기본 기능

--- UnityMobile/Assets/02.Scripts/Player/PlayerManager.cs
+++ UnityMobile/Assets/02.Scripts/Player/PlayerManager.cs
@@ -5,59 +5,138 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     #region 변수
-    //이동
-    private float Playerspeed;
-    private float h;
-    
-    //점프
-    private bool isJump = false;
-    private float Jumppower;
-
     //컴포넌트
     private Transform tr;
-    Rigidbody2D rigid;
+    private Rigidbody2D rigid;
+    private Animator anim;
 
+    //이동
+    private float input_x;
+    private float moveSpeed;
+    private float isRight;
+
+    //점프
+    private float input_jump;
+    private bool isJump;
+    private float jumpPower;
+
+    //애니매이션
+    private bool isRun;
+
+    //공격
+    private bool input_Attack;
+    private int attackCount;
     #endregion
 
-    private void Awake()
+    void Awake()
     {
-        //값
-        Playerspeed = 100f;
-        Jumppower = 10f;
-
         //컴포넌트
-        tr = this.GetComponent<Transform>();
-        rigid = this.GetComponent<Rigidbody2D>();
+        tr = GetComponent<Transform>();
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        //이동
+        moveSpeed = 10f;
+        isRight = 1;
+        isRun = false;
+
+        //점프
+        isJump = false;
+        jumpPower = 15;
+
+        //공격
+        attackCount = 0;
     }
 
     void Update()
     {
-        Move();
-        Jump();
+        InputSystem();
+        
     }
 
     void FixedUpdate()
     {
-        
+        Move();
+        Jump();
+        Attack();
     }
 
     #region 함수
+    void InputSystem()
+    {
+        input_x = Input.GetAxis("Horizontal");
+        input_jump = Input.GetAxis("Jump");
+        input_Attack = Input.GetKey(KeyCode.F);
+    }
+
     void Move()
     {
-        h = Input.GetAxis("Horizontal");
+        rigid.velocity = new Vector2(input_x * moveSpeed, rigid.velocity.y);
 
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Force);
+        if((input_x > 0 && isRight < 0) || (input_x < 0 && isRight > 0))
+        {
+            FlipPlayer();
+        }
+
+        if (input_x != 0)
+        {
+            isRun = true;
+        }
+        else
+        {
+            isRun = false;
+        }
+
+        anim.SetBool("isRun", isRun);
     }
 
     void Jump()
     {
-        isJump = Input.GetButtonDown("Jump");
-
-        if(isJump == true)
+        if(input_jump != 0 && isJump == false)
         {
-            rigid.AddForce(Vector2.up * Jumppower, ForceMode2D.Impulse);
+            rigid.velocity = Vector2.up * jumpPower;
+            isJump = true;
+            anim.SetTrigger("doJump");
         }
     }
 
+    void FlipPlayer()
+    {
+        tr.eulerAngles = new Vector3(0, Mathf.Abs(tr.eulerAngles.y - 180), 0);
+        isRight = isRight * -1;
+    }
+
+    void Attack()
+    {
+        if (input_Attack)
+            attackCount++;
+        else if (attackCount == 4)
+            attackCount = 1;
+
+        switch(attackCount)
+        {
+            case 1:
+                anim.SetInteger("isAttack", 1);
+                break;
+            case 2:
+                anim.SetInteger("isAttack", 2);
+                break;
+            case 3:
+                anim.SetInteger("isAttack", 3);
+                break;
+        }
+
+
+        
+    }
+
     #endregion
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJump = false;
+        }
+    }
 }
---
+++ UnityMobile/Assets/asd/Scripts/CameraMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+
+    [HideInInspector]
+    public Transform CameraLimit;
+
+    public Transform[] Limit;
+
+    float height;
+    float width;
+
+    void Start()
+    {
+        height = Camera.main.orthographicSize;
+        width = height * Screen.width / Screen.height;
+        ChangeLimit(0);
+    }
+
+    public void ChangeLimit(int x)
+    {
+        CameraLimit = Limit[x];
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * speed);
+
+        float lx = CameraLimit.localScale.x * 0.5f - width;
+        float clampX = Mathf.Clamp(transform.position.x, -lx + CameraLimit.position.x, lx + CameraLimit.position.x);
+
+        float ly = CameraLimit.localScale.y * 0.5f - height;
+        float clampY = Mathf.Clamp(transform.position.y, -ly + CameraLimit.position.y, ly + CameraLimit.position.y);
+
+        transform.position = new Vector3(clampX, clampY, -10f);
+    }
+}
